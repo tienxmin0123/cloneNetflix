e0b2040b86c9e75cd2db2fb277c5c6e8de22cf5b
tienxmin0123 <tienxmin0123@gmail.com>
2022-10-31 17:52:27 +0700

    Netflix : Fix css responsive card movie

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -7,6 +7,17 @@ import "../movieList/movieList.scss";
 export default function MovieList(props) {
   const { category, type, id } = props;
   const [dataMovie, setDataMovie] = useState(null);
+  // const [slideShow, setSlideShow] = useState(5);
+  // useEffect(() => {
+  //   (() => {
+  //     const clientWidth = document.querySelector("body").clientWidth;
+  //     if (clientWidth > 0 && clientWidth < 480) {
+  //       setSlideShow(2);
+  //     } else if (clientWidth >= 480 && clientWidth < 768) {
+  //       setSlideShow(4);
+  //     }
+  //   })();
+  // }, []);
   useEffect(() => {
     (async () => {
       let response = null;
@@ -23,7 +34,12 @@ export default function MovieList(props) {
   }, [category, type, id]);
   return (
     <>
-      <Swiper grabCursor={true} spaceBetween={10} slidesPerView={5}>
+      <Swiper
+        grabCursor={true}
+        spaceBetween={10}
+        slidesPerView={"auto"}
+        className="card__swiper"
+      >
         {dataMovie &&
           dataMovie.map((movie) => (
             <SwiperSlide key={movie.id}>
